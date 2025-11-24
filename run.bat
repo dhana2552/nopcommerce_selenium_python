30b374c697137059b5caa6cc92d7d9b456007f70
dhana2552 <dhana2552@gmail.com>
2025-11-24 21:03:32 +0530

    different python path

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 cd %WORKSPACE%
 
 echo Installing dependencies...
-"C:\Users\Dhanalakshmi Scal\AppData\Local\Microsoft\WindowsApps\python.exe" -m pip install pytest
-"C:\Users\Dhanalakshmi Scal\AppData\Local\Microsoft\WindowsApps\python.exe" -m pip install selenium
-"C:\Users\Dhanalakshmi Scal\AppData\Local\Microsoft\WindowsApps\python.exe" -m pip install undetected_chromedriver
-"C:\Users\Dhanalakshmi Scal\AppData\Local\Microsoft\WindowsApps\python.exe" -m pytest -v -s .\test_cases\
+D:\py311\python.exe -m pip install pytest
+D:\py311\python.exe -m pip install selenium
+D:\py311\python.exe -m pip install undetected_chromedriver
+D:\py311\python.exe -m pytest -v -s .\test_cases\
