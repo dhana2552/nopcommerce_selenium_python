8f2aa9e132578dc1ba92bbd0af209a355895382e
dhana2552 <dhana2552@gmail.com>
2025-11-24 18:48:21 +0530

    modified run.bat to have undetected_chromedriver installation

--- run.bat
+++ run.bat
@@ -4,4 +4,5 @@ cd %WORKSPACE%
 echo Installing dependencies...
 D:\Software\python.exe -m pip install pytest
 D:\Software\python.exe -m pip install selenium
+D:\Software\python.exe -m pip install undetected_chromedriver
 D:\Software\python.exe -m pytest -v -s .\test_cases\
